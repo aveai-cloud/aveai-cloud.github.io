2cd265f6c9d8b58b714e4ba34f878947a12826f1
root <root@ip-172-31-18-62.ap-southeast-1.compute.internal>
2025-09-03 13:15:59 +0000

    deploy

---
+++ assets/index-BwpzQKl2.js
@@ -0,0 +1,1 @@
+import{_ as r,x as s,h as a,o as c,r as _}from"./index-DT-wo-ax.js";const m={name:"Back",beforeRouteEnter(e,n,o){console.log("to.name ",e.name),e.name!=="Login"&&!s.state.bot.accessToken?o({name:"Login"}):o()}};function i(e,n,o,p,f,l){const t=_("router-view");return c(),a(t)}const d=r(m,[["render",i]]);export{d as default};
